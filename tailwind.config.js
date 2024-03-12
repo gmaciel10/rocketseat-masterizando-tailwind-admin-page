8492a9979c7df80c4b09715cce3a19fc3148173d
gmaciel10 <guilherme.maciel.9889@gmail.com>
2024-03-11 21:29:41 -0300

    Implementação da responsividade do formulário, sidebar e configuração do tema dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,26 @@
-const config = {
-  content: ['./src/**/*.tsx'],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
-        profile: "max-content 1fr min-content",
-        form: "minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0rem,15rem)"
+        form: 'minmax(7.5rem, 17.5rem), minmax(25rem, 1fr), minmax(0, 15rem)',
       },
+
       borderWidth: {
-        6: "6px"
+        6: '6px',
       },
+
       colors: {
         violet: {
-          25: '#fcfaff'
+          25: '#FCFAFF',
         },
+
         error: {
           25: '#FFFBFA',
           50: '#FEF3F2',
@@ -26,30 +33,26 @@ const config = {
           700: '#B42318',
           800: '#912018',
           900: '#7A271A',
-        }
+        },
       },
+
       keyframes: {
-        slideDownAnFade: {
+        slideDownAndFade: {
           from: { opacity: 0, transform: 'translateY(-2px)' },
           to: { opacity: 1, transform: 'translateY(0)' },
         },
-        slideUpAnFade: {
-          from: { opacity: 1 },
-          to: { opacity: 0 },
-        }
-      },
-      animation: {
-        slideDownAnFade: 'slideDownAnFade 0.4s cubic-bezier(0.16,1,0.3,1)',
-        slideUpAnFade: 'slideUpAnFade 1s linear'
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        slideUpAndFade: {
+          from: { opacity: 0, transform: 'translateY(2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
       },
 
+      animation: {
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar'), require('@tailwindcss/forms')],
 }
-export default config
